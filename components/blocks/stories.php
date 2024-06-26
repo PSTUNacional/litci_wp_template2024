53f98f31a694bba2740a99acf8ddb76ccb85236b
romerito.pontes <romerito.pontes@monitoratec.com.br>
2024-06-26 16:10:39 -0300

    CSS Fix and Parties logos

--- components/blocks/stories.php
+++ components/blocks/stories.php
@@ -18,62 +18,62 @@
             </div>
             <div class="party-grid">
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('<?=get_template_directory()?>/assets/img/parties/pstu.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/pstu.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-br fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/pstuar.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/pstuar.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-ar fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/corrienteroja.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/corrienteroja.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-es fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/mit.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/mit.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-cl fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/pst.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/pst.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-co fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/pt.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/pt.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-cr fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/pdac.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/pdac.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-it fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/pct.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/pct.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-sv fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/emluta.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/emluta.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-pt fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/pstpe.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/pstpe.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-pe fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/ptpy.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/ptpy.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-py fis"></span>
                 </a>
                 <a class="party-item">
-                    <div class="logo" style="background-image:url('/wp-content/themes/litci/assets/img/parties/psthn.jpg')"></div>
+                    <div class="logo" style="background-image:url('<?=get_template_directory_uri()?>/assets/img/parties/psthn.jpg')"></div>
                     <div class="circle"></div>
                     <span class="fi fi-hn fis"></span>
                 </a>
