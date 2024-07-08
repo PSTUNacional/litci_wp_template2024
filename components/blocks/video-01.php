c6207b9187a5678f566008b7ba410f9c57d20280
romerito.pontes <romerito.pontes@monitoratec.com.br>
2024-07-08 09:59:15 -0300

    Video block update for show each channel

--- components/blocks/video-01.php
+++ components/blocks/video-01.php
@@ -3,6 +3,25 @@ $url = 'https://videos.litci.org/api/videos';
 $json = file_get_contents($url);
 $videos = json_decode($json, TRUE);
 $videos = $videos['data'];
+$groupedChannels = [];
+foreach($videos as $video)
+{
+    $channelId = $video['channel_id'];
+    if (!isset($groupedVideos[$channelId])) {
+        $groupedVideos[$channelId] = [];
+    }
+    $groupedChannels[$channelId][] = $video;
+}
+
+$videos = [];
+for($i = 0; $i < 3; $i++)
+{
+    foreach($groupedChannels as $c)
+    {
+        $videos[] = $c[$i];
+    }
+}
+
 ?>
 <section class="video-block-01 video-section">
     <div class="container">
@@ -14,10 +33,10 @@ $videos = $videos['data'];
     <div class="container">
         <div class="column ta-left video-grid">
             <div class="video-item" data-video="<?= $videos[0]['video_id'] ?>">
-                <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[0]['video_id'] ?>/hqdefault.jpg')"></div>
+                <a href="<?=$videos[0]['url']?>" class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[0]['video_id'] ?>/hqdefault.jpg')"></a>
                 <div class="video-info">
                     <!--<span class="badge primary"></span>-->
-                    <h3><?= $videos[0]['title'] ?></h3>
+                    <a href="<?=$videos[0]['url']?>"><h3><?= $videos[0]['title'] ?></h3></a>
                     <span class="date"><?= date("d M Y", strtotime($videos[0]['publication_date'])) ?></span>
                     <!--<p class="description"><?= $videos[0]['description'] ?></p>-->
                     <!--<p class="duration"><?= $videos[0]['duration'] ?></p>-->
@@ -26,10 +45,10 @@ $videos = $videos['data'];
         </div>
         <div class="column video-grid">
             <div class="video-item" data-video="<?= $videos[1]['video_id'] ?>">
-                <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[1]['video_id'] ?>/hqdefault.jpg')"></div>
+                <a href="<?=$videos[1]['url']?>" class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[1]['video_id'] ?>/hqdefault.jpg')"></a>
                 <div class="video-info">
                     <!--<span class="badge primary"></span>-->
-                    <h3><?= $videos[1]['title'] ?></h3>
+                    <a href="<?=$videos[1]['url']?>"><h3><?= $videos[1]['title'] ?></h3></a>
                     <span class="date"><?= date("d M Y", strtotime($videos[1]['publication_date'])) ?></span>
                     <!--<p class="description"><?= $videos[1]['description'] ?></p>-->
                     <!--<p class="duration"><?= $videos[1]['duration'] ?></p>-->
@@ -43,10 +62,10 @@ $videos = $videos['data'];
             for($i=2; $i<6; $i++)
             { ?>
                 <div class="video-item" data-video="<?= $videos[$i]['video_id'] ?>">
-                    <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[$i]['video_id'] ?>/hqdefault.jpg')"></div>
+                    <a href="<?=$videos[$i]['url']?>" class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[$i]['video_id'] ?>/hqdefault.jpg')"></a>
                     <div class="video-info">
                         <!--<span class="badge primary"><?= $videos[$i]['channel_name'] ?></span>-->
-                        <h3><?= $videos[$i]['title'] ?></h3>
+                        <a href="<?=$videos[$i]['url']?>"><h3><?= $videos[$i]['title'] ?></h3></a>
                         <span class="date"><?= date("d M Y", strtotime($videos[$i]['publication_date'])) ?></span>
                         <!--<p class="description"><?= $videos[$i]['description'] ?></p>-->
                         <!--<p class="duration"><?= $videos[$i]['duration'] ?></p>-->
@@ -58,24 +77,3 @@ $videos = $videos['data'];
     </div>
     </div>
 </section>
-<script>
-    videoheader = document.querySelector('.video-destak');
-    video = document.querySelectorAll('.video-item')[0].dataset['video']
-
-    videoItems = document.querySelectorAll('.video-item')
-    videoItems.forEach(el => {
-        el.addEventListener('click', () => {
-            videoId = event.currentTarget.getAttribute('data-video')
-            title = event.currentTarget.querySelector('h3').innerText
-            description = event.currentTarget.querySelector('.description').innerText
-            duration = event.currentTarget.querySelector('.duration').innerText
-
-            videoheader.querySelector('.video-thumb').style.backgroundImage = "url('http://i3.ytimg.com/vi/" + videoId + "/maxresdefault.jpg')"
-            videoheader.querySelector('h1').innerText = title
-            videoheader.querySelector('#videoDescription').innerText = description
-            videoheader.querySelector('#videoDuration span').innerText = duration
-            video = videoId
-            window.scrollTo(0, 0);
-        })
-    })
-</script>
