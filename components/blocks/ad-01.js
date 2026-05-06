83dc076e653441eca9dd5f29ab5feef371f6b4f1
Romerito Pontes <romeritovp@gmail.com>
2026-05-06 19:38:29 -0300

    Ad block fix

--- components/blocks/ad-01.js
+++ components/blocks/ad-01.js
@@ -4,8 +4,8 @@
     var SelectControl = components.SelectControl;
     var InspectorControls = editor.InspectorControls;
     var MediaUpload = editor.MediaUpload;
-    var URLInput = editor.URLInput;
     var Button = components.Button;
+    var PanelBody = components.PanelBody;
 
     const icon = el('img', {src:'../wp-content/themes/litci/components/blocks/icons/block01.svg'})
 
@@ -26,6 +26,10 @@
                 type: 'string',
                 default: null,
             },
+            backgroundImageMobile: {
+                type: 'string',
+                default: null,
+            },
             link: {
                 type: 'string',
                 default: '',
@@ -39,6 +43,10 @@
                 props.setAttributes({ backgroundImage: media.url });
             };
 
+            var onSelectImageMobile = function (media) {
+                props.setAttributes({ backgroundImageMobile: media.url });
+            };
+
             var onChangeLink = function (newLink) {
                 props.setAttributes({ link: newLink });
             };
@@ -49,52 +57,77 @@
                 props.setAttributes({ backgroundColor: newColor, isDark: isDark });
             };
 
-            return el('div', { className: "block-card", style:{}},
-                attributes.backgroundImage ? el('img', { src: attributes.backgroundImage, style: { width: '100%' } }) : el('div', {}, 'Nenhuma imagem selecionada'),
-                el(InspectorControls, {},
-                    el('div', { className: "block-editor-block-card" },
-                        el(MediaUpload, {
-                            onSelect: onSelectImage,
-                            allowedTypes: ['image'],
-                            render: function (obj) {
-                                return el(Button, {
-                                    className: attributes.backgroundImage ? 'image-button' : 'button button-large',
-                                    onClick: obj.open
-                                }, !attributes.backgroundImage ? 'Selecione uma imagem de fundo' : 'Alterar imagem de fundo');
-                            }
-                        })
+            // Helper: renderiza um campo de imagem com título, preview e botão
+            var renderImageField = function (title, currentImage, onSelect) {
+                return el('div', { className: 'litci-image-field' },
+                    el('p', { className: 'litci-image-field__title' }, title),
+                    currentImage && el('div', { className: 'litci-image-field__preview' },
+                        el('img', { src: currentImage })
                     ),
-                    el(TextControl, {
-                        className: "block-editor-block-card",
-                        label: 'Link',
-                        value: attributes.link,
-                        onChange: onChangeLink
-                    }),
-                    el(SelectControl, {
-                        className: "block-editor-block-card",
-                        label: 'Cor de Fundo',
-                        value: attributes.backgroundColor,
-                        options: [
-                            { label: 'White', value: '#ffffff' },
-                            { label: 'Gray 50', value: '#f8f8f8' },
-                            { label: 'Gray 100', value: '#eaeaea' },
-                            { label: 'Gray 200', value: '#d8d8d8' },
-                            { label: 'Gray 300', value: '#bababa' },
-                            { label: 'Gray 400', value: '#aaaaaa' },
-                            { label: 'Gray 500', value: '#9b9b9b' },
-                            { label: 'Gray 600', value: '#666666' },
-                            { label: 'Gray 700', value: '#565656' },
-                            { label: 'Gray 800', value: '#474747' },
-                            { label: 'Gray 900', value: '#323232' },
-                            { label: 'Gray 950', value: '#222222' },
-                        ],
-                        onChange: onChangeBackgroundColor
+                    el(MediaUpload, {
+                        onSelect: onSelect,
+                        allowedTypes: ['image'],
+                        render: function (obj) {
+                            return el(Button, {
+                                className: 'litci-image-field__button',
+                                variant: currentImage ? 'secondary' : 'primary',
+                                onClick: obj.open
+                            }, !currentImage ? 'Selecionar imagem' : 'Alterar imagem');
+                        }
                     })
+                );
+            };
+
+            var previewSrc = attributes.backgroundImage || attributes.backgroundImageMobile;
+
+            return el('div', { className: "block-card", style:{}},
+                previewSrc 
+                    ? el('img', { src: previewSrc, style: { width: '100%' } }) 
+                    : el('div', {}, 'Nenhuma imagem selecionada'),
+                el(InspectorControls, {},
+                    el(PanelBody, { title: 'Imagens do banner', initialOpen: true },
+                        renderImageField(
+                            'Imagem desktop',
+                            attributes.backgroundImage,
+                            onSelectImage
+                        ),
+                        renderImageField(
+                            'Imagem mobile',
+                            attributes.backgroundImageMobile,
+                            onSelectImageMobile
+                        )
+                    ),
+                    el(PanelBody, { title: 'Configurações', initialOpen: true },
+                        el(TextControl, {
+                            label: 'Link',
+                            value: attributes.link,
+                            onChange: onChangeLink
+                        }),
+                        el(SelectControl, {
+                            label: 'Cor de fundo',
+                            value: attributes.backgroundColor,
+                            options: [
+                                { label: 'White', value: '#ffffff' },
+                                { label: 'Gray 50', value: '#f8f8f8' },
+                                { label: 'Gray 100', value: '#eaeaea' },
+                                { label: 'Gray 200', value: '#d8d8d8' },
+                                { label: 'Gray 300', value: '#bababa' },
+                                { label: 'Gray 400', value: '#aaaaaa' },
+                                { label: 'Gray 500', value: '#9b9b9b' },
+                                { label: 'Gray 600', value: '#666666' },
+                                { label: 'Gray 700', value: '#565656' },
+                                { label: 'Gray 800', value: '#474747' },
+                                { label: 'Gray 900', value: '#323232' },
+                                { label: 'Gray 950', value: '#222222' },
+                            ],
+                            onChange: onChangeBackgroundColor
+                        })
+                    )
                 )
             );
         },
         save: function () {
-            return null; // Não é necessário salvar nada no frontend
+            return null;
         },
     });
 })(
--- components/blocks/ad-01.php
+++ components/blocks/ad-01.php
@@ -1,10 +1,21 @@
 <?php
+$desktop = $attributes['backgroundImage'] ?? '';
+$mobile  = $attributes['backgroundImageMobile'] ?? '';
+$link    = $attributes['link'] ?? '#';
+
+// Fallback: se uma das duas faltar, usa a outra
+$desktopSrc = $desktop ?: $mobile;
+$mobileSrc  = $mobile  ?: $desktop;
 ?>
 <section>
     <div class="container">
         <div class="column">
-            <a class="ads" href="<?= $attributes['link'] ?>">
-                <img src="<?= $attributes['backgroundImage'] ?>" />
+            <a class="ads" href="<?= esc_url($link) ?>">
+                <picture>
+                    <source media="(max-width: 768px)" srcset="<?= esc_url($mobileSrc) ?>">
+                    <source media="(min-width: 769px)" srcset="<?= esc_url($desktopSrc) ?>">
+                    <img src="<?= esc_url($desktopSrc) ?>" alt="" loading="lazy">
+                </picture>
             </a>
         </div>
     </div>
